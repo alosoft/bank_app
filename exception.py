003bd48f9a8be7182467a2b7bca37e6981792266
Raymond Adutwum Agyei <raymondadutwum@gmail.com>
2018-09-08 19:23:34 +0000

    added double quotes strings description to exception functions

--- exception.py
+++ exception.py
@@ -1,17 +1,17 @@
 def num_there(s):
-    # check if user input contains numbers
+    """ check if user input contains numbers and Returns True if Number exits"""
     return any(i.isdigit() for i in s)
 
 
 def numb_warning():
-    # print warning if user input number
+    """print warning if user input number"""
     print('*' * 28)
     print('Input should be ONLY alphabets')
     print('*' * 28)
 
 
 def alpha_warning():
-    # print warning if user input word
+    """print warning if user input word"""
     print('*' * 46)
     print('Input should be ONLY digits')
     print('*' * 46)
